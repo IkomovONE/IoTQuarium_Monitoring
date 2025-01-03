baecb309fb4e8c2ef612a8a1a7b06bb37a8742f6
Daniil Komov <daniil_komov@icloud.com>
2025-01-03 05:35:53 +0200

    allow origins

--- backend/server.py
+++ backend/server.py
@@ -40,7 +40,7 @@ db = Mongoclient["IoT_Quarium_Monitoring"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="http://localhost:8080",
+    allow_origins="*",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers="*",
