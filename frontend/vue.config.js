c9b336fea1cc1e45ff093be3a9d97c76aeff3ba6
IkomovONE <daniil_komov@icloud.com>
2025-01-12 23:26:16 +0200

    fixed host setting

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/",
   devServer: {
+    allowedHosts: [
+      'iotquarium.info',  // Allow requests from this domain
+    ],
+    
     host: '0.0.0.0', // Bind to all interfaces
     port: 8080,
   },
