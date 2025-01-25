9158cdb826bf1c9dd2159adb02c96143596c6828
IkomovONE <daniil_komov@icloud.com>
2025-01-25 12:58:46 +0200

    Fixed bug related to OLED process. Added new thread

--- backend/server.py
+++ backend/server.py
@@ -47,7 +47,7 @@ limiter = Limiter(key_func=get_remote_address)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins="*",  #https://iotquarium.info
+    allow_origins="https://iotquarium.info",  #https://iotquarium.info
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers="*",
@@ -93,7 +93,7 @@ def save_msg(i):
 
 def input_data():
 
-    OLED()
+    
 
     #Make database entry
 
@@ -232,8 +232,11 @@ def start_data_generation():
     global input_thread, daily_thread
     input_thread = threading.Thread(target=input_data, daemon=True)
     daily_thread = threading.Thread(target=daily_data_input, daemon=True)
+    oled_thread = threading.Thread(target=OLED, daemon=True)
     input_thread.start()
     daily_thread.start()
+    oled_thread.start()
+    
 
 
 #####
